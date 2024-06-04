876d39f8191393e1ad4548ea452e379b68145027
Shin Umeda <57329636+DolphinGui@users.noreply.github.com>
2024-06-03 20:32:35 -0700

    Block diagram (#2)
    * Added github docs deployment ci
    
    * added whole system block diagram
    
    * fixing deploy code to use remotes, since branches aren't in local
    upon fetching
    
    * strip origin from refname
    
    * Retrieve cache before doing everything

--- generate.sh
+++ generate.sh
@@ -3,8 +3,8 @@
 set -ex
 
 eval $(git for-each-ref --shell \
-  --format='git checkout %(objectname) && poetry run mike deploy %(refname:lstrip=2);' \
-  --exclude=refs/remotes/* --exclude=refs/heads/gh-pages  \
-   refs/heads)
+  --format='git checkout %(objectname) && poetry run mike deploy %(refname:lstrip=3);' \
+  --exclude=refs/remotes/origin/gh-pages  \
+   refs/remotes/origin)
 poetry run mike set-default main
 git checkout gh-pages
