d14621cf1b105a6edc8a31be015fd81190a1db45
Shin Umeda <umeda.shin@gmail.com>
2024-06-03 19:50:52 -0700

    fixing deploy code to use remotes, since branches aren't in local upon fetching

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ set -ex
 
 eval $(git for-each-ref --shell \
   --format='git checkout %(objectname) && poetry run mike deploy %(refname:lstrip=2);' \
-  --exclude=refs/remotes/* --exclude=refs/heads/gh-pages  \
-   refs/heads)
+  --exclude=refs/remotes/origin/gh-pages  \
+   refs/remotes/origin)
 poetry run mike set-default main
 git checkout gh-pages
